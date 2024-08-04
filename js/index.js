2c3138a78e227b3847bdc01505b921fa37db97c8
Monica Magxwalisa <monmagx@gmail.com>
2024-08-04 12:39:31 +0200

    updated index files

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const navtoggle= document.querySelector('.nav-toggle');
+const navlinks= document.querySelectorAll('.nav__link');
+
+
+navtoggle.addEventListener('click',()=>{
+    document.body.classList.toggle('nav-open');
+});
+
+navlinks.forEach(link=>{
+    links.addEventListener('click',()=>{
+        document.body.classList.remove('nav-open');
+    })
+ });
+
+ 
